4a49ded6ade9450099e53da09f70068cb65b1b52
Jonathan Wang <jonathan.wang01@sap.com>
2022-06-04 19:48:04 +0800

    change the oauth client

--- src/main/java/com/example/oauth2/client/config/ProjectConfig.java
+++ src/main/java/com/example/oauth2/client/config/ProjectConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
@@ -20,21 +21,21 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
 //    private ClientRegistration clientRegistration() {
 //        return CommonOAuth2Provider.GITHUB.getBuilder("github")
-//                .clientId("a7553955a0c534ec5e6b")
-//                .clientSecret("1795b30b425ebb79e424afa51913f1c724da0dbb")
+//                .clientId("da77db2512b589933d87")
+//                .clientSecret("3e063c11e388feacc09c08e9a872d186239039ea")
 //                .build();
 //    }
 
         private ClientRegistration clientRegistration() {
-        return ClientRegistration.withRegistrationId("github")
-                .clientId("da77db2512b589933d87")
-                .clientSecret("3e063c11e388feacc09c08e9a872d186239039ea")
-                .scope(new String[]{"read:user"})
-                .authorizationUri("https://github.com/login/oauth/authorize")
-                .tokenUri("https://github.com/login/oauth/access_token")
-                .userInfoUri("https://api.github.com/user")
+        return ClientRegistration.withRegistrationId("client")
+                .clientId("client")
+                .clientSecret("secret")
+                .scope(new String[]{"read"})
+                .authorizationUri("http://auth-server:8080/oauth/authorize")
+                .tokenUri("http://auth-server:8080/oauth/token")
                 .userNameAttributeName("id")
-                .clientName("GitHub")
+                .userInfoUri("http://auth-server:8080/user")
+                .clientName("client")
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .redirectUriTemplate("{baseUrl}/{action}/oauth2/code/{registrationId}")
                 .build();
@@ -42,7 +43,7 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.oauth2Login();
+        http.oauth2Login().userInfoEndpoint();
 
         http.authorizeRequests()
                 .anyRequest().authenticated();
